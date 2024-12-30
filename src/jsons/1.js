ab9e66c1f7da268cc9178fa66aaa26dffdbed3d9
mathyber <ilan23f@gmail.com>
2024-12-30 19:49:47 +0300

    2024 fix list

--- src/jsons/1.js
+++ src/jsons/1.js
@@ -1,158 +1,110 @@
 export default `
-pyrokinesis
-Дворцовый мост
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/42/f6/0e/42f60e26-8e47-a9d5-cb2c-a373ce6d876e/cover.jpg/316x316bb.webp
+Clara
+DIAMANTI GREZZI
+https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/88/3e/bd/883ebd55-8bbb-689c-fdbe-c849fad2d805/5054197953019.jpg/316x316bb.webp
 
 DEATHPIXIE
 SAFE WORD
 https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c1/77/b3/c177b367-87ce-e9ea-d15b-b6e1c867352c/0.jpg/316x316bb.webp
 
-
-KSHMR
-Happy (feat. Tiina)
-https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/ae/66/8d/ae668d51-ccfa-9dfa-243b-38ff7ad17671/5054197997273.jpg/316x316bb.webp
-
-
-Studio Seven, Danimal, Heleen
-Alive
-https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/2f/7a/08/2f7a0835-9aef-00b2-e0c5-bdc06aaa8f64/artwork.jpg/316x316bb.webp
-
-Kordhell, Anar
- JUNBI
-https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3b/a0/75/3ba0751a-0180-3702-feaa-ac059bd89551/198588109717.jpg/316x316bb.webp
-
-
-
-Kordhell, SADFRIENDD
- I Don't Really Give a Fuck
-https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/35/df/26/35df26f7-35ea-704e-edeb-8cb29f6decfd/198588643686.jpg/316x316bb.webp
-
-
-Скрэт
-ММК
-https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/c0/8c/74/c08c7419-5515-f4ba-3dec-0309e8939012/5063316412313.jpg/316x316bb.webp
-
-
-Lastfragment
- Miss or Hate You, Рt. 2
-https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/4b/02/21/4b022125-d50d-6878-f6af-acaa3137f9ec/cover.jpg/316x316bb.webp
-
-
-DEKADANCE
-Верность
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/c6/d0/5d/c6d05dd6-e07d-a7fe-060f-c81bc0ec7fc5/cover.jpg/316x316bb.webp
-
-
-Горе-Музыкант
-Этот Трек Тебе Не Понравится 2.0
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/75/49/61/7549618b-96cb-36b7-3466-49fb7a95f0b0/730530248350_cover.jpg/316x316bb.webp
-
-
-PRXSXNT FXTURE, KXRAIN
-CRUEL
-https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/80/1e/0b/801e0b9d-cda0-e01f-d100-ecc86642eebf/198391270727.jpg/316x316bb.webp
-
-
-Oxxxymiron
-МИР ГОРИТ
-https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c6/15/a8/c615a84c-cd96-aebe-e44e-4b82ed97a9bf/198588075630.jpg/316x316bb.webp
-
-
-Sarah Bonnici
-Lose
-https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/19/9e/1c/199e1c9a-b78a-3e35-adec-c1eea72edd32/artwork.jpg/316x316bb.webp
-
+MoonDeity
+SEX
+https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/fc/43/f6/fc43f689-6956-2849-c785-025ae22d8e65/198391419546.jpg/316x316bb.webp
 
 Dargen D'Amico
 Onda Alta
 https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/10/26/0a/10260a11-1429-bb77-b842-e5336e3946c5/24UMGIM05837.rgb.jpg/316x316bb.webp
 
-
-Annalisa
-Sinceramente
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/6e/fe/af/6efeaf67-47cc-3808-da77-2f7d36ad0805/5054197949418.jpg/316x316bb.webp
-
-
-Clara
-DIAMANTI GREZZI
-https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/88/3e/bd/883ebd55-8bbb-689c-fdbe-c849fad2d805/5054197953019.jpg/316x316bb.webp
-
-
-Angelina Mango
-La noia
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/b0/40/72/b0407227-7416-6644-2818-77c89c74183a/197338113561.jpg/316x316bb.webp
-
-
-Baby Lasagna
-Rim Tim Tagi Dim
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/bf/bc/41/bfbc411d-b7d1-b4c9-782e-81a1c9e92917/24UMGIM02921.rgb.jpg/316x316bb.webp
-
-
-TALI
-Fighter
-https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/04/ef/de/04efdee5-7440-e133-be7c-dcce4f1e2409/cover.jpg/316x316bb.webp
-
-
-5MIINUST x Puuluup
-(nendest) narkootikumidest ei tea me (küll) midagi
-https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/88/1b/98/881b98be-a5b9-b2ce-0d52-af4ceb04edda/24UMGIM73779.rgb.jpg/316x316bb.webp
-
-
 Sarah Bonnici
 Loop
 https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/75/4e/e0/754ee09d-6da2-88fa-8365-29e4c02ac36b/artwork.jpg/316x316bb.webp
 
-Kaleen
-We Will Rave
-https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/97/ee/b7/97eeb7b8-c55c-78ce-a8f6-82adf99b5e2f/5021732239532.jpg/316x316bb.webp
+Kordhell, SADFRIENDD
+I Don't Really Give a Fuck
+https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/35/df/26/35df26f7-35ea-704e-edeb-8cb29f6decfd/198588643686.jpg/316x316bb.webp
 
-
-Eden Golan
-Hurricane
-https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/65/d8/bc/65d8bc61-8bc5-c55a-6a0a-9690fc9829d8/810129998528.jpg/316x316bb.webp
-
-
-OLLIE
-My Friend
-https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3b/15/fc/3b15fca7-c381-0caf-5264-44b3ac949df9/0.jpg/316x316bb.webp
-
-
-Maria Sur
-When I'm Gone
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/ef/15/bb/ef15bba6-b22a-8372-59f2-2209be350165/5054197962783.jpg/316x316bb.webp
-
-
-Jaxx & Vega & VSNS
-Euphoria
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/5e/d0/8d/5ed08da0-a748-97a8-09d2-55de9ffee9a0/3689.jpg/316x316bb.webp
-
+Kordhell, Anar
+JUNBI
+https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3b/a0/75/3ba0751a-0180-3702-feaa-ac059bd89551/198588109717.jpg/316x316bb.webp
 
 plagueinside
 Танцы
 https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/d4/b1/25/d4b12534-3590-a2c3-e2f0-5c1e71a8a625/0.jpg/316x316bb.webp
 
-SCARLET
-Circus X
-https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/e8/5e/8a/e85e8a07-c759-39e1-40f8-b773bcff5769/196922838637_Cover.jpg/316x316bb.webp
-
-KEiiNO
-Damdiggida
-https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/f6/e7/a1/f6e7a1c7-fe20-d0b4-9139-2ceeb7275116/5054197939716.jpg/316x316bb.webp
-
 Gothminister
 We Come Alive
 https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/b8/d4/6b/b8d46baa-ba4a-e960-95b6-53fcb6a642a1/cover.jpg/316x316bb.webp
 
-FORBIDDEN
-ARCTURUS
-https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/97/69/36/976936ba-fda9-ecd4-d906-4ce977253247/cover.jpg/316x316bb.webp
+Скрэт
+ММК
+https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/c0/8c/74/c08c7419-5515-f4ba-3dec-0309e8939012/5063316412313.jpg/316x316bb.webp
 
-MoonDeity
-SEX
-https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/fc/43/f6/fc43f689-6956-2849-c785-025ae22d8e65/198391419546.jpg/316x316bb.webp
+Lastfragment
+Miss or Hate You, Рt. 2
+https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/4b/02/21/4b022125-d50d-6878-f6af-acaa3137f9ec/cover.jpg/316x316bb.webp
 
-BENNETT feat. Mentissa
-Mamma Mia (Techno Mix)
-https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/93/21/f8/9321f8f5-d42d-d11d-9571-c5598c602b8a/5021732446916.jpg/316x316bb.webp
+PRXSXNT FXTURE, KXRAIN
+CRUEL
+https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/80/1e/0b/801e0b9d-cda0-e01f-d100-ecc86642eebf/198391270727.jpg/316x316bb.webp
+
+Oxxxymiron
+МИР ГОРИТ
+https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c6/15/a8/c615a84c-cd96-aebe-e44e-4b82ed97a9bf/198588075630.jpg/316x316bb.webp
+
+Baby Lasagna
+Rim Tim Tagi Dim
+https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/bf/bc/41/bfbc411d-b7d1-b4c9-782e-81a1c9e92917/24UMGIM02921.rgb.jpg/316x316bb.webp
+
+TALI
+Fighter
+https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/04/ef/de/04efdee5-7440-e133-be7c-dcce4f1e2409/cover.jpg/316x316bb.webp
+
+5MIINUST x Puuluup
+(nendest) narkootikumidest ei tea me (küll) midagi
+https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/88/1b/98/881b98be-a5b9-b2ce-0d52-af4ceb04edda/24UMGIM73779.rgb.jpg/316x316bb.webp
+
+Kaleen
+We Will Rave
+https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/97/ee/b7/97eeb7b8-c55c-78ce-a8f6-82adf99b5e2f/5021732239532.jpg/316x316bb.webp
+
+Eden Golan
+Hurricane
+https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/65/d8/bc/65d8bc61-8bc5-c55a-6a0a-9690fc9829d8/810129998528.jpg/316x316bb.webp
+
+OLLIE
+My Friend
+https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3b/15/fc/3b15fca7-c381-0caf-5264-44b3ac949df9/0.jpg/316x316bb.webp
+
+Maria Sur
+When I'm Gone
+https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/ef/15/bb/ef15bba6-b22a-8372-59f2-2209be350165/5054197962783.jpg/316x316bb.webp
+
+Jaxx & Vega & VSNS
+Euphoria
+https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/5e/d0/8d/5ed08da0-a748-97a8-09d2-55de9ffee9a0/3689.jpg/316x316bb.webp
+
+DEKADANCE
+Верность
+https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/c6/d0/5d/c6d05dd6-e07d-a7fe-060f-c81bc0ec7fc5/cover.jpg/316x316bb.webp
+
+KSHMR
+Happy (feat. Tiina)
+https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/ae/66/8d/ae668d51-ccfa-9dfa-243b-38ff7ad17671/5054197997273.jpg/316x316bb.webp
+
+Sarah Bonnici
+Lose
+https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/19/9e/1c/199e1c9a-b78a-3e35-adec-c1eea72edd32/artwork.jpg/316x316bb.webp
+
+SCARLET
+Circus X
+https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/e8/5e/8a/e85e8a07-c759-39e1-40f8-b773bcff5769/196922838637_Cover.jpg/316x316bb.webp
+
+Annalisa
+Sinceramente
+https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/6e/fe/af/6efeaf67-47cc-3808-da77-2f7d36ad0805/5054197949418.jpg/316x316bb.webp
+
+Studio Seven, Danimal, Heleen
+Alive
+https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/2f/7a/08/2f7a0835-9aef-00b2-e0c5-bdc06aaa8f64/artwork.jpg/316x316bb.webp
 
 `
